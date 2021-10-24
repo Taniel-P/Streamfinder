02f3ecf9034605e7d5b6126225d45b66dc52fd50
Mark P Thomas <markums@gmail.com>
2021-10-24 00:10:40 -0600

    Added error boundary component and example usage

---
+++ client/features/sharedComponents/ErrorBoundary.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: '',
+      errorInfo: '',
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log({ error, errorInfo });
+    this.setState({ errorInfo });
+  }
+
+  render() {
+    const { hasError, errorInfo } = this.state;
+    if (hasError) {
+      return (
+        <div className="component">
+          <div className="component-header">
+            <p>
+              There was an error in loading this page.{' '}
+              <span
+                style={{ cursor: 'pointer', color: '#0077FF' }}
+                onClick={() => {
+                  window.location.reload();
+                }}
+              >
+                Reload this page
+              </span>{' '}
+            </p>
+          </div>
+          <div className="component-body">
+            <details className="error-details">
+              <summary>Click for error details</summary>
+              {errorInfo && errorInfo.componentStack.toString()}
+            </details>
+          </div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+ErrorBoundary.propTypes = {
+  children: PropTypes.oneOfType([ PropTypes.object, PropTypes.array ]).isRequired,
+};
+
+export default ErrorBoundary;
