6326deee6e88385b658581296ce90409f5f37467
Mark P Thomas <markums@gmail.com>
2021-10-26 00:28:12 -0600

    Setup basic Jest-Enzyme testing

---
+++ client/features/sharedComponents/starRatingInteractive.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import StarRatingInteractive from './StarRatingInteractive.jsx';
+
+// ==== Test Template ====
+// ====== index.jsx ======
+// For Jest usage, see: https://jestjs.io/docs/getting-started
+// For Enzyme usage, see: https://github.com/enzymejs/enzyme-matchers/tree/master/packages/jest-enzyme
+
+describe('Test Component entry point', function () {
+  it('renders without crashing given the required props', () => {
+    const wrapper = shallow(<StarRatingInteractive />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
