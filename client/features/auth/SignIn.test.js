f6a56b3c40ea357cf8ee41eeed9246b4925e46f2
Taniel Pogharian <taniel.pogharian@gmail.com>
2021-10-28 19:22:00 -0700

    Added Tests

--- client/features/auth/SignIn.test.js
+++ client/features/auth/SignIn.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-// import SignIn from './SignIn.jsx';
+import SignIn from './SignIn.jsx';
 
 // ==== Test Template ====
 // ====== index.jsx ======
@@ -11,7 +11,7 @@ import toJson from 'enzyme-to-json';
 
 describe('Test Component entry point', function () {
   it('renders without crashing given the required props', () => {
-    // const wrapper = shallow(<SignIn />);
-    // expect(toJson(wrapper)).toMatchSnapshot();
+    const wrapper = shallow(<SignIn />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
