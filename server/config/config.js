e82ab4a96baa916b0f6414b55734894e97fe7c37
stevenharderjr <48738599+stevenharderjr@users.noreply.github.com>
2021-10-31 15:28:47 -0600

    Comment out .env exports

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
-require('dotenv').config({ path: `${__dirname}/.env` });
+// require('dotenv').config({ path: `${__dirname}/.env` });
 
-const { env } = process;
+// const { env } = process;
 
-module.exports = {
-	name: env.APP_NAME,
-	baseUrl: env.APP_BASE_URL,
-  port: env.PORT,
-  port_other: env.PORT_OTHER,
-  redis_host: env.REDIS_HOST,
-  redis_port: env.REDIS_PORT,
-  redis_password: env.REDIS_PASSWORD,
-}
+// module.exports = {
+// 	name: env.APP_NAME,
+// 	baseUrl: env.APP_BASE_URL,
+//   port: env.PORT,
+//   port_other: env.PORT_OTHER,
+//   redis_host: env.REDIS_HOST,
+//   redis_port: env.REDIS_PORT,
+//   redis_password: env.REDIS_PASSWORD,
+// }
