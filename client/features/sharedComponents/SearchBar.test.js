ef6909975528598e678ad9cbfc8ccef100a7c2e1
Steven Harder <stevenharderjr@gmail.com>
2021-11-06 09:11:31 -0600

    Fixes SearchBar test after renaming event handler

--- client/features/sharedComponents/SearchBar.test.js
+++ client/features/sharedComponents/SearchBar.test.js
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar.jsx';
 describe('Test SearchBar component', () => {
   it('Should call its parent\'s handleSearch function when a search is submitted', () => {
     const mockSubmit = jest.fn();
-    const component = shallow(<SearchBar placeholder={'test'} handleSearch={mockSubmit} />);
+    const component = shallow(<SearchBar placeholder={'test'} onSearch={mockSubmit} />);
     component.setState({ searchTerm: 'test' });
     component.find('form#ss-search-form').simulate('submit', { preventDefault: () => {} });
     expect(mockSubmit).toHaveBeenCalled();
