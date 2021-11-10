a4475316451d7d6d637a7fc57709dde3fb8dc3a5
Mark P Thomas <markums@gmail.com>
2021-11-09 20:33:21 -0700

    Removed saving of movie history to user documents in the DB

--- server/search/searchDB.js
+++ server/search/searchDB.js
@@ -7,16 +7,12 @@ const {
   getFlatRateProviders,
   getMovieWithProviders,
   addProvidersToMovies } = require('./searchHelpers');
-const {
-  transformHistoryResponse,
-  transformSuggestedResponse } = require('../home/movieHelpers'); // TODO: Review more later
+const { transformSuggestedResponse } = require('../home/movieHelpers');
 const {
   getMovie: getMovieAPI,
   getTrending: getTrendingAPI,
   getSuggested: getSuggestedAPI,
   getProviders: getProvidersAPI } = require('./APIController');
-
-// Jaimie - TODO:  if you could somehow figure out how NOT to save the search history to the history part of the user database, that would be fantastic!
 
 module.exports = {
   getMovie: (movieName, user) => {
@@ -46,93 +42,60 @@ module.exports = {
               getSuggestedAPI(movieId).then((suggestedMovies) => {
                 const uniqueMovieIds = getUniqueIds(searchedMovie, trendingMovies, suggestedMovies);
                 // TODO: Below is the result of Promise.All from above
-                // const providers = getProvidersAPI(uniqueMovieIds);
-                // providers.then((providersByMovie) => {
-                getProvidersAPI(uniqueMovieIds).then((providersByMovie) => {
+                getProvidersAPI(uniqueMovieIds)
+                .then((providersByMovie) => {
                   const flatRateProvidersByMovie = getFlatRateProviders(providersByMovie);
                   // Logger.consoleLog('finalProviders: ', flatRateProvidersByMovie);
 
                   const movieWithProviders = getMovieWithProviders(searchedMovie, flatRateProvidersByMovie);
 
-                  // replace with real user function once running
-                  // TODO: What is the state of this?
-                  const checkAndUpdate = User.find({ username: user }).then((userData) => {
-                    // TODO: What were the intentions of user.History? Jaimie was wanting some aspect of this removed
-                    // Logger.consoleLog(userData)
-                    if (userData[0].history.length) {
-                      userData[0].history.forEach((searchedMovie) => {
-                        if (searchedMovie[0].title.toLowerCase() !== movieWithProviders.title.toLowerCase()) {
-                          User.updateOne({
-                            username: user,
-                            $push: { history: movieWithProviders },
-                            currentId: movieWithProviders.id
-                          },
-                          (err, data) => {
-                            Logger.consoleLog('Saved New Record: User History');
-                          });
-                        } else {
-                          Logger.consoleLog('Record Exists');
-                        }
-                      });
-                    } else if (!userData[0].history.length) {
-                      User.updateOne({username: user, $push: {history: movieWithProviders}}, (err, data) => {
-                        Logger.consoleLog('Saved New Record: MovieSchema');
-                      });
-                    }
-                    return true;
-                  });
-                  checkAndUpdate.then((bool) => {
-                    // TODO: User.find is currently not doing anything with the resulting query data
-                    User.find({ username: user }, (err, data) => {
-                      movieWithProviders.trending = addProvidersToMovies(trendingMovies, flatRateProvidersByMovie);
-                      movieWithProviders.suggested = addProvidersToMovies(suggestedMovies, flatRateProvidersByMovie);
-                      const saveMovieToDB = (movie) => {
-                        let filter = { id: movie.id };
+                  movieWithProviders.trending = addProvidersToMovies(trendingMovies, flatRateProvidersByMovie);
+                  movieWithProviders.suggested = addProvidersToMovies(suggestedMovies, flatRateProvidersByMovie);
 
-                        let update = {
-                          suggested,
-                          trending,
-                          mediaType,
-                          title,
-                          rating,
-                          ratingCount,
-                          summary,
-                          imgUrl,
-                          hulu,
-                          disney,
-                          netflix,
-                          hbo,
-                          apple,
-                          amazon
-                        } = movie;
+                  const saveMovieToDB = (movie) => {
+                    let filter = { id: movie.id };
 
-                        let options = {
-                          new: true,
-                          upsert: true
-                        };
-                        Movie.findOneAndUpdate(filter, update, options, (err, data) => {
-                          if (err) {
-                            Logger.consoleLog('ERROR in movieSave: ', err);
-                          } else {
-                            Logger.consoleLog('SUCCESS saving movie: ');//, data);
-                          }
-                        });
-                      };
+                    let update = {
+                      suggested,
+                      trending,
+                      mediaType,
+                      title,
+                      rating,
+                      ratingCount,
+                      summary,
+                      imgUrl,
+                      hulu,
+                      disney,
+                      netflix,
+                      hbo,
+                      apple,
+                      amazon
+                    } = movie;
 
-                      saveMovieToDB(movieWithProviders);
-                      const movieSearchResponse = transformSuggestedResponse({ suggested: movieWithProviders.suggested });
-                      Logger.consoleLog('movieSearchResponse:', movieSearchResponse, '🚀');
-                      // TODO: Let this run, but on later PR
-                      // movie.history = movieHistoryResponse
-                      // Logger.consoleLog(movie.history)
-                      resolve(movieSearchResponse);
+                    let options = {
+                      new: true,
+                      upsert: true
+                    };
+                    Movie.findOneAndUpdate(filter, update, options, (err, data) => {
+                      if (err) {
+                        Logger.consoleLog('ERROR in movieSave: ', err);
+                      } else {
+                        Logger.consoleLog('SUCCESS saving movie: ');//, data);
+                      }
                     });
+                  };
 
-                    // TODO: save (key) name and (value) id to redis
-                    // TODO: Exit as below instead of resolve(movieSearchResponse) above?
-                    // const finalSearch = transformToSearchDisplay(movieSearchResponse);
-                    // resolve(finalSearch);
-                  });
+                  saveMovieToDB(movieWithProviders);
+                  const movieSearchResponse = transformSuggestedResponse({ suggested: movieWithProviders.suggested });
+                  Logger.consoleLog('movieSearchResponse:', movieSearchResponse, '🚀');
+
+                  // TODO: Let this run, but on later PR switch to next section for 'resolve'
+                  resolve(movieSearchResponse);
+
+                  // TODO: save (key) name and (value) id to redis
+                  // TODO: Exit as below instead of resolve(movieSearchResponse) above?
+                  // const finalSearch = transformToSearchDisplay(movieSearchResponse);
+                  // resolve(finalSearch);
                 });
               });
             });
